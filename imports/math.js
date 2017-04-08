353340f78076e71edc25f7c35501611c70244315
George Higbie <gehigbie@gmail.com>
2017-04-07 17:48:13 -0700

    Create addNumbers function

---
+++ imports/math.js
@@ -0,0 +1,1 @@
+export let addNumbers = (a, b) => a+b; 
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 // import "./../imports/utils";
 import {greetUser, name} from "./../imports/utils";
+import {addNumbers} from "./../imports/math";
 
 console.log("Log from /server/main.js");
 console.log(greetUser(name));
+console.log(addNumbers(4, 5));
