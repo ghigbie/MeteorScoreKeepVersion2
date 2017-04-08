9ca192a0bb8799ea618dc17f0f855a43d3d7e5f5
George Higbie <gehigbie@gmail.com>
2017-04-08 10:58:52 -0700

    Add user to metero.startup

--- server/main.js
+++ server/main.js
@@ -3,5 +3,9 @@ import {Players} from "./../imports/api/players";
 
 
 Meteor.startup( () => {
-
-}); 
+    Players.insert({
+        name: "Jeff",
+        score: 33
+    });
+    console.log(Players.find().fetch()); //find returns a curser and fetch returns an array of documents
+});
