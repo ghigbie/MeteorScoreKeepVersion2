ba95b56ec50f885ead26ff35ae63ac74ff40106c
George Higbie <gehigbie@gmail.com>
2017-04-07 18:55:55 -0700

    Add and render jsx to main.html

--- client/main.js
+++ client/main.js
@@ -1,3 +1,8 @@
 import React from "react"; // react only has one default export, which is called react
-import ReactDom from "react-dom"; //this is the same situation for react-dom
+import ReactDOM from "react-dom"; //this is the same situation for react-dom
 import {Meteor} from "meteor/meteor"; //this is a named export
+
+Meteor.startup( () => {
+    let jsx = <p>This is from JSX!!!</p>;
+    ReactDOM.render(jsx, document.getElementById("app")); //ReactDom takes two arguments: the first is the item you wish to render, and the second is the location to which you wish to render
+});
