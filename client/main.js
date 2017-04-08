04385ef482af2a7525ccaa7cc0828100087c5136
George Higbie <gehigbie@gmail.com>
2017-04-07 18:40:16 -0700

    Install React and React-Dom

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 // import "./../imports/utils";
 import someDefault, {greetUser, name} from "./../imports/utils";
-import {addNumbers} from "./../imports/math";
+import defaultFunction, {addNumbers} from "./../imports/math";
 
 console.log("Log from /client/main.js");
 console.log(greetUser(name));
 console.log(addNumbers(7, 3));
 console.log(someDefault);
+console.log(defaultFunction(9));
