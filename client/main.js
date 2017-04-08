f5f6b7893a6323efe5cc51d8a905e5effa8c1c27
George Higbie <gehigbie@gmail.com>
2017-04-07 19:25:14 -0700

    Inject data

--- client/main.js
+++ client/main.js
@@ -2,7 +2,36 @@ import React from "react"; // react only has one default export, which is called
 import ReactDOM from "react-dom"; //this is the same situation for react-dom
 import {Meteor} from "meteor/meteor"; //this is a named export
 
+const players = [
+    {
+        _id: "1",
+        name: "Lauren",
+        score: 50
+    },
+    {
+        _id: "2",
+        name: "Josh",
+        score: 78
+    },
+    {
+        _id: "3",
+        name: "Beth",
+        score: 67
+    }
+];
+const renderPlayers = () => {
+    return [<p key="1">1</p>, <p key="2">2</p>, <p key="3">3</p> ]
+};
+
 Meteor.startup( () => {
-    let jsx = <p>This is from JSX!!!</p>;
+    let title = "Score Keep";
+    let name = "Mike";
+    let jsx = (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>Hello {name}</p>
+            <p>Se dice que no se puede escapar la ocean.</p>
+            {renderPlayers()}
+        </div>);
     ReactDOM.render(jsx, document.getElementById("app")); //ReactDom takes two arguments: the first is the item you wish to render, and the second is the location to which you wish to render
 });
