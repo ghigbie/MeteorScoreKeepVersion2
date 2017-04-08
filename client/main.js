ff3ffa23e9a22cdb7c91b722f3aa20f966e53a85
George Higbie <gehigbie@gmail.com>
2017-04-07 17:49:33 -0700

    Import addNumbers function

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 // import "./../imports/utils";
 import {greetUser, name} from "./../imports/utils";
+import {addNumbers} from "./../imports/math";
 
 console.log("Log from /client/main.js");
 console.log(greetUser(name));
+console.log(addNumbers(7, 3));
