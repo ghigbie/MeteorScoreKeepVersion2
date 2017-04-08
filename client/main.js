414a7c2a4dcc290ac79c65a51b2326bb7316fcd9
George Higbie <gehigbie@gmail.com>
2017-04-08 11:51:23 -0700

    Add function to load db data to page

--- client/main.js
+++ client/main.js
@@ -4,27 +4,6 @@ import {Meteor} from "meteor/meteor"; //this is a named export
 import {Players} from "./../imports/api/players"; //this will create a mini mongo dtabase
 import {Tracker} from "meteor/tracker";
 
-Tracker.autorun( () => {
-    console.log("players List", Players.find().fetch());
-});
-
-const players = [
-    {
-        _id: "1",
-        name: "Lauren",
-        score: 50
-    },
-    {
-        _id: "2",
-        name: "Josh",
-        score: 78
-    },
-    {
-        _id: "3",
-        name: "Beth",
-        score: 67
-    }
-];
 const renderPlayers = (playersList) => {
     let points = "points";
     return playersList.map((player) => {
@@ -36,13 +15,17 @@ const renderPlayers = (playersList) => {
 };
 
 Meteor.startup( () => {
+    let players = [];
+    Tracker.autorun(() => {
+        players = Players.find().fetch();
+    });
     let title = "Score Keep";
     let name = "Mike";
     let jsx = (
         <div className="container">
             <h1>{title}</h1>
             <p>Hello {name}</p>
-            <p>Se dice que no se puede escapar la ocean.</p>
+            <p>Se dice que no se puede escapar la oceana.</p>
             {renderPlayers(players)}
         </div>);
     ReactDOM.render(jsx, document.getElementById("app")); //ReactDom takes two arguments: the first is the item you wish to render, and the second is the location to which you wish to render
